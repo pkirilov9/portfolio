ba9b023f20133d01cd6bb8b6b9ca8c354bc95a8d
Petar Kirilov <petarkirilov@Petars-MacBook-Pro.local>
2024-09-28 14:05:55 +0300

    28/09_6

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import * as Component from "./quartz/styles/custom.scss"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
