6ee9de6b4a708c78674b62733c87ff66af7043d2
Petar Kirilov <petarkirilov@Petars-MacBook-Pro.local>
2024-09-28 14:12:52 +0300

    changing_config.ts_test

--- quartz.config.ts
+++ quartz.config.ts
@@ -61,6 +61,8 @@ const config: QuartzConfig = {
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      additionalCss: ["/assets/css/custom.css"], // Add this line to link your custom CSS
+      ),
       Plugin.ContentPage(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
